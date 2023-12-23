967dc21513df90925af0e035dec447a8ebc1e7f6
Adam J. Richardson <fatman.uk@gmail.com>
2023-12-23 15:47:38 +0000

    Add docs for ansible_vault module

--- ansible_vault.py
+++ ansible_vault.py
@@ -7,23 +7,49 @@ __metaclass__ = type
 
 DOCUMENTATION = r'''
 ---
+module: 'ansible_vault'
+short_description: 'Manipulate Ansible Vaults.'
+description:
+  - 'Manipulate various functions of the Ansible Vault.'
+version_added: '0.1'
+options:
+  mode:
+    description:
+      - 'The mode in which to execute.'
+      - 'Current modes: encrypt.'
+    type: 'str'
+    required: yes
+requirements:
+  - 'installed Linux system'
+  - 'ansible'
+author:
+  - 'Adam J. Richardson (@FatmanUK)'
+extends_documentation_fragment:
+    - 'action_common_attributes'
+attributes:
+    check_mode:
+        support: 'full'
+    diff_mode:
+        support: 'full'
+    platform:
+        support: 'full'
+        platforms: 'posix'
 '''
 
 EXAMPLES = r'''
+- name: 'Make a plain text file into an Ansible Vault with password
+        from password_file.'
+  ansible_vault:
+    mode: 'encrypt'
+    password_file: '../{{ ansible_vault_secret }}'
+    src: '{{ ansible_vault_random_filename.path }}'
+    dest: '../{{ ansible_vault_file }}'
 '''
 
 RETURN = r'''#'''
 
-#import os
-#import platform
-#import pwd
-#import re
-#import sys
-#import tempfile
-
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.common.text.converters import to_native
-#from ansible.module_utils.six.moves import shlex_quote
 
 def main():
     module = AnsibleModule(
@@ -35,16 +61,7 @@ def main():
     res_args = dict()
     warnings = list()
 
-
-    res_args = dict(
-        warnings=warnings,
-        changed=changed
-    )
-
-    module.exit_json(**res_args)
-
-if __name__ == '__main__':
-    main()
+# TODO: when more functions crop up, split into ansible_encrypt and so on?
 
 #- name: 'Encrypt the lot'
 #  ansible_vault:
@@ -61,4 +78,14 @@ if __name__ == '__main__':
 #      - '{{ ansible_vault_random_filename.path }}'
 #      - '--output'
 #      - '../{{ ansible_vault_file }}'
+
+    res_args = dict(
+        warnings=warnings,
+        changed=changed
+    )
+
+    module.exit_json(**res_args)
+
+if __name__ == '__main__':
+    main()
 
