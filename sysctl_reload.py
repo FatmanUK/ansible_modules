f5b0b35372d9d11599375c9ae78c3bbc83625b1e
Adam J. Richardson <fatman.uk@gmail.com>
2024-01-02 19:54:25 +0000

    Remove unneeded copyright notice and Python2 compat stuff

--- sysctl_reload.py
+++ sysctl_reload.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2023, Adam J. Richardson <fatman.uk@gmail.com>
-
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-
 DOCUMENTATION = r'''
 ---
 module: 'sysctl_reload'
